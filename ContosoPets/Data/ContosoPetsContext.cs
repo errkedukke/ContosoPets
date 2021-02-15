8c990d164bf0038ffb170a760395859e3917a7b2
Guga Rukhadze <38571128+errkedukke@users.noreply.github.com>
2021-02-15 15:52:40 +0400

    Add project files.

---
+++ ContosoPets/Data/ContosoPetsContext.cs
@@ -0,0 +1,22 @@
+﻿using ContosoPets.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ContosoPets.Data
+{
+    class ContosoPetsContext : DbContext
+    {
+        public DbSet<Customer> Customers { get; set; }
+        public DbSet<Order> Orders { get; set; }
+        public DbSet<Product> Products { get; set; }
+        public DbSet<ProductOrder> ProductOrders { get; set; }
+    
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder) 
+        {
+            optionsBuilder.UseSqlServer("Data Source=(localdb)\\MSSQLLocalDB;Initial Catalog = ContosoPets;");
+        }
+    }
+}
---
+++ ContosoPets/Program.cs
@@ -0,0 +1,54 @@
+﻿using ContosoPets.Data;
+using ContosoPets.Models;
+using System;
+using System.Linq;
+
+namespace ContosoPets
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //ContextAddInfo();
+            ContextReadInfo();
+        }
+
+        static void ContextAddInfo() 
+        {
+            using var context = new ContosoPetsContext();
+
+            var squeakyBone = new Product()
+            {
+                Name = "Squeaky Bone Dog",
+                Price = 4.99M
+            };
+
+            var tennisBall = new Product()
+            {
+                Name = "Tennis ball 3-Pack",
+                Price = 9.99M
+            };
+
+            context.Add(squeakyBone);
+            context.Add(tennisBall);
+
+            context.SaveChanges();
+        }
+        static void ContextReadInfo()
+        {
+            using var context = new ContosoPetsContext();
+
+            var products = context.Products
+                .Where(p => p.Price >= 5.00m)
+                .OrderBy(p => p.Name);
+
+            foreach(var product in products) 
+            {
+                Console.WriteLine("Id: " + product.Id);
+                Console.WriteLine("Name: " + product.Name);
+                Console.WriteLine("Price: " + product.Price);
+                Console.WriteLine(new string('-', 20));
+            }
+        }
+    }
+}
